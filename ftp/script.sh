993d1c87bff97909d99f7cbb014230d85a3a8cdf
tiana <tiana.randrianarivony@gmail.com>
2024-09-20 16:02:52 +0300

    pull csv remote

--- ftp/script.sh
+++ ftp/script.sh
@@ -5,20 +5,10 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
 # Ajouter la clé d'hôte SFTP au fichier known_hosts
-ssh-keyscan -H "52.47.200.235" >> ~/.ssh/known_hosts
+ssh-keyscan -H "$SFTP_HOST" >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
 echo "pulling csv...."
-
-# Variables de connexion SFTP
-SFTP_HOST="52.47.200.235"
-SFTP_USER="riester"
-SFTP_PASSWORD="jmwIsm8VzDHxktz"
-REMOTE_DIR="/home/riester"
-LOCAL_DIR="./data/riester"
-
-# Vérifier que le répertoire local existe
-
 
 # Connexion SFTP avec sshpass
 sshpass -p "$SFTP_PASSWORD" sftp -o StrictHostKeyChecking=no $SFTP_USER@$SFTP_HOST <<EOF
