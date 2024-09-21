3d261a04301a70d16747fa77d04a03e89d35ed82
tiana <tiana.randrianarivony@gmail.com>
2024-09-21 12:02:40 +0300

    code review

--- ftp/converter.sh
+++ ftp/converter.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 LOCAL_DIR="../data/riester"
-echo "ICII $LOCAL_DIR"
-# Vérifier si des fichiers CSV ont été récupérés
 if ls "$LOCAL_DIR"/*.csv 1> /dev/null 2>&1; then
   echo "conversion des fichiers en séparateur à virgules :"
 
-  # Boucle sur tous les fichiers CSV récupérés dans le répertoire local
   for file in "$LOCAL_DIR"/*.csv; do
     if [ -f "$file" ]; then
-      echo "Traitement du fichier : $file"
+      echo "Traitement du fichier : $file \n"
+      echo "Suppression de la première ligne"
       sed -i '1d' "$file"
       # Remplacer le séparateur de tabulation par des virgules directement dans le fichier original
       sed -i 's/\t/,/g' "$file"
