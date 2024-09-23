04759bfde6e247815e2fc568044aeb9d67bbf4af
tiana <tiana.randrianarivony@gmail.com>
2024-09-23 12:28:14 +0300

    handle csv

--- ftp/script.sh
+++ ftp/script.sh
@@ -17,3 +17,22 @@ lcd $LOCAL_DIR
 mget *.csv
 bye
 EOF
+echo "Fichiers récupérés dans $LOCAL_DIR :"
+
+echo "handle csv"
+if ls "$LOCAL_DIR"/*.csv 1> /dev/null 2>&1; then
+  echo "conversion des fichiers en séparateur à virgules :"
+
+  for file in "$LOCAL_DIR"/*.csv; do
+    if [ -f "$file" ]; then
+      echo "Traitement du fichier : $file \n"
+      echo "Suppression de la première ligne"
+      sed -i '1d' "$file"
+      # Remplacer le séparateur de tabulation par des virgules directement dans le fichier original
+      sed -i 's/\t/,/g' "$file"
+      echo "Fichier converti et remplacé : $file"
+    fi
+  done
+else
+  echo "Aucun fichier CSV trouvé dans $LOCAL_DIR"
+fi
