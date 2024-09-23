8bf1bfbf3bbc4f1eb0fc4381f32bc8da8439de9e
tiana <tiana.randrianarivony@gmail.com>
2024-09-23 14:04:18 +0300

    pull remote data

--- ftp/script.sh
+++ ftp/script.sh
@@ -3,14 +3,14 @@
 # Créer le répertoire .ssh s'il n'existe pas et s'assurer des bonnes permissions
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-
+echo "go to $SFTP_HOST"
 # Ajouter la clé d'hôte SFTP au fichier known_hosts
 ssh-keyscan -H "$SFTP_HOST" >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
 echo "pulling csv...."
-
-# Connexion SFTP avec sshpass
+#
+## Connexion SFTP avec sshpass
 sshpass -p "$SFTP_PASSWORD" sftp -o StrictHostKeyChecking=no $SFTP_USER@$SFTP_HOST <<EOF
 cd $REMOTE_DIR
 lcd $LOCAL_DIR
@@ -26,6 +26,8 @@ if ls "$LOCAL_DIR"/*.csv 1> /dev/null 2>&1; then
   for file in "$LOCAL_DIR"/*.csv; do
     if [ -f "$file" ]; then
       echo "Traitement du fichier : $file \n"
+      echo "FIX ENCODING"
+      iconv -f ISO-8859-1 -t UTF-8 $file -o $file
       echo "Suppression de la première ligne"
       sed -i '1d' "$file"
       # Remplacer le séparateur de tabulation par des virgules directement dans le fichier original
