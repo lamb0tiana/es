24bde40d7856db6d295b11aeed84840d47a8b574
tiana <tiana.randrianarivony@gmail.com>
2024-09-24 15:38:35 +0300

    set script modulable

--- ftp/script.sh
+++ ftp/script.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
 
-# Créer le répertoire .ssh s'il n'existe pas et s'assurer des bonnes permissions
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-echo "go to $SFTP_HOST"
-# Ajouter la clé d'hôte SFTP au fichier known_hosts
-ssh-keyscan -H "$SFTP_HOST" >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
-if [ -d "$LOCAL_DIR/riester" ]; then
-  echo "Suppression du contenu du répertoire $LOCAL_DIR/riester"
-  rm -rf "$LOCAL_DIR/riester"/*
-else
-  echo "Le répertoire $LOCAL_DIR/riester n'existe pas. on passe"
-fi
-echo "pulling csv...."
-#
-## Connexion SFTP avec sshpass
-sshpass -p "$SFTP_PASSWORD" sftp -o StrictHostKeyChecking=no $SFTP_USER@$SFTP_HOST <<EOF
+setup_ssh() {
+  mkdir -p ~/.ssh
+  chmod 700 ~/.ssh
+  echo "Go to $SFTP_HOST"
+  ssh-keyscan -H "$SFTP_HOST" >> ~/.ssh/known_hosts
+  chmod 644 ~/.ssh/known_hosts
+}
+
+manage_directory() {
+  mkdir -p "$LOCAL_DIR/riester"
+  rm -rf "$LOCAL_DIR/riester"/* # clean last files
+  mkdir -p "$TEMP_DIR"
+}
+
+pull_csv_files() {
+  echo "Pulling CSV..."
+  sshpass -p "$SFTP_PASSWORD" sftp -o StrictHostKeyChecking=no "$SFTP_USER@$SFTP_HOST" <<EOF
 cd $REMOTE_DIR
 lcd $TEMP_DIR
 mget *.csv
 bye
 EOF
-echo "Fichiers récupérés dans $TEMP_DIR :"
+  echo "Fichiers récupérés dans $TEMP_DIR :"
+}
 
-echo "handle csv"
-if ls "$TEMP_DIR"/*.csv 1> /dev/null 2>&1; then
-  echo "conversion des fichiers en séparateur à virgules :"
+process_csv_files() {
+  echo "Handle CSV"
+  if ls "$TEMP_DIR"/*.csv 1> /dev/null 2>&1; then
+    echo "Convert csv seprator to comma"
+    for file in "$TEMP_DIR"/*.csv; do
+      if [ -f "$file" ]; then
+        echo "FIX ENCODING"
+        iconv -f ISO-8859-1 -t UTF-8 "$file" -o "$file"
+        echo "REMOVE UNNECESSARY FIRST LINE"
+        sed -i '1d' "$file"
+        sed -i 's/\t/,/g' "$file"
+        echo "handled file > : $file"
+      fi
+    done
+  else
+    echo "no file found to $TEMP_DIR"
+  fi
+}
 
-  for file in "$TEMP_DIR"/*.csv; do
-    if [ -f "$file" ]; then
-      echo "Traitement du fichier : $file \n"
-      echo "FIX ENCODING"
-      iconv -f ISO-8859-1 -t UTF-8 $file -o $file
-      echo "Suppression de la première ligne"
-      sed -i '1d' "$file"
-      # Remplacer le séparateur de tabulation par des virgules directement dans le fichier original
-      sed -i 's/\t/,/g' "$file"
-      echo "Fichier converti et remplacé : $file"
-    fi
-  done
-else
-  echo "Aucun fichier CSV trouvé dans $TEMP_DIR"
-fi
-mv $TEMP_DIR/* $LOCAL_DIR/riester/
+move_processed_files() {
+  mkdir -p "$LOCAL_DIR/riester/"
+  mv "$TEMP_DIR"/* "$LOCAL_DIR/riester/"
+}
+
+setup_ssh
+manage_directory
+pull_csv_files
+process_csv_files
+move_processed_files
