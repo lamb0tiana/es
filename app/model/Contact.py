fceb3a15b7f76d9adfa87939e572a7b5c7974309
tiana <tiana.randrianarivony@gmail.com>
2024-10-02 11:55:20 +0300

    update contact

--- app/model/Contact.py
+++ app/model/Contact.py
@@ -1,9 +1,18 @@
-from pydantic import create_model
-from typing import Optional
+from pydantic import create_model, Field, EmailStr
+
+from typing import Optional, Callable, Any
 
 from app.lib.constant import riesterDBColumns
 
-dynamic_fields = {name: (Optional[str], None) for name in riesterDBColumns}
+
+def get_field_type(name: str) -> Callable[[], Any]:
+    if "email" in name:
+        return (Optional[EmailStr], Field(None, description=f"{name} de contact"))
+    else:
+        return (Optional[str], Field(None, description=f"{name} de contact"))
+
+
+dynamic_fields = {name: get_field_type(name) for name in riesterDBColumns}
 
 
 class Config:
